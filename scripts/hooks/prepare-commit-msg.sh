833139f5b5715039a5b425ddf09377bc703b1dfb
devlinjunker <devlin.junker@gmail.com>
2019-12-22 17:02:58 -0800

    wip: notes

--- scripts/hooks/prepare-commit-msg.sh
+++ scripts/hooks/prepare-commit-msg.sh
@@ -6,10 +6,11 @@ read -ra PARAM_ARRAY <<< "$HUSKY_GIT_PARAMS"
 set COMMIT_TYPE = "${PARAM_ARRAY[1]}"
 
 if [ "$COMMIT_TYPE" == "template" ]; then
-  echo "nothing?";
+  echo "Could ask type and save to prefix message with after added in editor";
 elif [ "$COMMIT_TYPE" == "message" ]; then
-  echo "TODO: ask type and prefix message with it";
+  echo "TODO: ask type and save to prefix message with";
 else
   echo $COMMIT_TYPE;
+  # during `--amend` this will be empty
 fi
 
