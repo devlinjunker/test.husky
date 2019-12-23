a495d88159d5f3df9c7fe6ebc1e392586c839da6
devlinjunker <devlin.junker@gmail.com>
2019-12-22 16:37:33 -0800

    wip: started on conditional based on commit type

--- scripts/hooks/prepare-commit-msg.sh
+++ scripts/hooks/prepare-commit-msg.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-echo $HUSKY_GIT_PARAMS;
+
+read -ra PARAM_ARRAY <<< "$HUSKY_GIT_PARAMS"
+
+if [ "${PARAM_ARRAY[1]}" == "template" ]; then
+  echo "nothing?";
+elif [ "${PARAM_ARRAY[1]}" == "message" ]; then
+  echo "TODO: ask type and prefix message with it";
+fi
+
